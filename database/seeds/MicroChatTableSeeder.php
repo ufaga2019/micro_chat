07a3a2b6bb22e7d169982da896e0d5c23d38a7c1
Alekskiper <sashatujh123@gmail.com>
2019-01-23 14:45:00 +0500

    upd

--- database/seeds/MicroChatTableSeeder.php
+++ database/seeds/MicroChatTableSeeder.php
@@ -9,22 +9,5 @@ class MicroChatTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        \App\Models\MicroChats::create([
-         'object_id' =>'1',
-         'message'=>'New Message',
-         'alias'=>'client',
-         'by'=>'\app\Models\MicroChats',
 
-        ]);
-
-        \App\Models\MicroChats::create([
-            'object_id' =>'2',
-            'message'=>'Last Message',
-            'alias'=>'client',
-            'by'=>'\app\Models\MicroChats',
-
-        ]);
-    }
 }
